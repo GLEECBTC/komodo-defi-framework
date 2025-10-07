82d7a3a2cd7b73777f75ff5d13dcd818096e2c55
Onur Özkan <work@onurozkan.dev>
2025-10-07 11:39:08 +0300

    handle token programs on token init
    Signed-off-by: Onur Özkan <work@onurozkan.dev>

--- mm2src/coins/solana/solana_token.rs
+++ mm2src/coins/solana/solana_token.rs
@@ -128,32 +128,36 @@ impl SolanaToken {
             &protocol_info.mint_address,
         );
 
-        // TODO: Handle non-existent mint accounts, then uncomment this code.
-        //
-        // let rpc = platform_coin.rpc_client().await.map_err(|e| SolanaTokenInitError {
-        //     ticker: ticker.clone(),
-        //     kind: SolanaTokenInitErrorKind::UnhealthyRPCs,
-        // })?;
+        let rpc = platform_coin.rpc_client().await.map_err(|e| SolanaTokenInitError {
+            ticker: ticker.clone(),
+            kind: SolanaTokenInitErrorKind::UnhealthyRPCs,
+        })?;
 
-        // let mint_account = rpc
-        //     .get_account(&protocol_info.mint_address)
-        //     .map_err(|e| SolanaTokenInitError {
-        //         ticker: ticker.clone(),
-        //         kind: SolanaTokenInitErrorKind::QueryError { reason: e.to_string() },
-        //     })?;
-
-        // if mint_account.owner != spl_token_program::id() {
-        //     return MmError::err(SolanaTokenInitError {
-        //         ticker: ticker.clone(),
-        //         kind: SolanaTokenInitErrorKind::QueryError {
-        //             reason: format!(
-        //                 "Unsupported SPL program. Expected Program ID: '{}', Got: '{}'.",
-        //                 spl_token_program::id(),
-        //                 mint_account.owner
-        //             ),
-        //         },
-        //     });
-        // }
+        match rpc.get_account(&protocol_info.mint_address) {
+            Ok(mint_account) => {
+                if mint_account.owner != spl_token_program::id() {
+                    return MmError::err(SolanaTokenInitError {
+                        ticker: ticker.clone(),
+                        kind: SolanaTokenInitErrorKind::QueryError {
+                            reason: format!(
+                                "Unsupported SPL program. Expected Program ID: '{}', Got: '{}'.",
+                                spl_token_program::id(),
+                                mint_account.owner
+                            ),
+                        },
+                    });
+                }
+            },
+            Err(e) if e.kind.to_string().contains("AccountNotFound") => {
+                // Nothing to do here.
+            },
+            Err(e) => {
+                return MmError::err(SolanaTokenInitError {
+                    ticker: ticker.clone(),
+                    kind: SolanaTokenInitErrorKind::QueryError { reason: e.to_string() },
+                })
+            },
+        };
 
         let token_fields = SolanaTokenFields {
             ticker,
